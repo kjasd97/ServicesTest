816de173e557a3b474e608d9e10c762e17650594
Roman Ulyanenko <ulyanenkorr@gmail.com>
2023-04-17 11:05:28 +0300

    Initial commit

---
+++ app/src/main/java/com/ulyanenko/servicestest/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.ulyanenko.servicestest
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.ulyanenko.servicestest.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private val binding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+    }
+}
